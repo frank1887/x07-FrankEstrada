957aed0751127764d45af4de14d12c288bdc310d
student <student@softeng>
2017-02-27 12:07:58 -0800

    Add x05 tests

--- TicTacToeBoard.cpp
+++ TicTacToeBoard.cpp
@@ -2,18 +2,22 @@
 /**
  * Class for representing a 3x3 Tic-Tac-Toe game board, using the Piece enum
  * to represent the spaces on the board.
-**/
+ **/
 
 //Switches turn member variable to represent whether it's X's or O's turn
 void TicTacToeBoard::toggleTurn()
 {
+	if(turn == X)
+		turn = O;
+	else
+		turn = X;
 
 }
 
 //Constructor sets an empty board and specifies it is X's turn first
 TicTacToeBoard::TicTacToeBoard()
 {
-
+	turn = X;
 }
 
 //Resets each board location to the Blank Piece value
@@ -28,26 +32,44 @@ void TicTacToeBoard::clearBoard()
  * NOT allow to place a piece in a location where there is already a piece.
  * In that case, placePiece just returns what is already at that location. 
  * Out of bounds coordinates return the Piece Invalid value
-**/ 
+ **/ 
 Piece TicTacToeBoard::placePiece(int row, int column)
 {
-  return Invalid;
+	Piece toReturn;
+	if(row > 2)
+	{
+		toReturn = Invalid;
+	}
+	else
+	{
+		if(turn == X)
+		{
+			toggleTurn();
+			toReturn = X;
+		}
+		else if(turn == O)
+		{
+			toggleTurn();
+			toReturn = O;
+		}
+	}
+	return toReturn;
 }
 
 /**
  * Returns what piece is at the provided coordinates, or Blank if there
  * are no pieces there, or Invalid if the coordinates are out of bounds
-**/
+ **/
 Piece TicTacToeBoard::getPiece(int row, int column)
 {
-  return Invalid;
+	return Invalid;
 }
 
 /**
  * Returns which Piece has won, if there is a winner, Invalid if the game
  * is not over, or Blank if the board is filled and no one has won.
-**/
+ **/
 Piece TicTacToeBoard::getWinner()
 {
-  return Invalid;
+	return Invalid;
 }
--- TicTacToeBoardTest.cpp
+++ TicTacToeBoardTest.cpp
@@ -1,10 +1,13 @@
 /**
  * Unit Tests for TicTacToeBoard
-**/
+ **/
 
 #include <gtest/gtest.h>
 #include "TicTacToeBoard.h"
- 
+#include <iostream>
+#include <string>
+using namespace std;
+
 class TicTacToeBoardTest : public ::testing::Test
 {
 	protected:
@@ -18,3 +21,65 @@ TEST(TicTacToeBoardTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
 }
+
+TEST(TicTacToeBoardTest, startingOffWithX)
+{
+	TicTacToeBoard test;
+	Piece starting = test.placePiece(0, 0);
+	bool actual;
+	if(starting == X)
+		actual = true;
+	else if(starting == O)
+		actual = false;
+	ASSERT_TRUE(actual);
+}
+
+TEST(TicTacToeBoardTest, switchingToO)
+{
+	TicTacToeBoard test;
+	Piece next = test.placePiece(0, 0);
+	next = test.placePiece(0, 0);
+	bool shouldBeO;
+	if(next == O)
+		shouldBeO = true;
+	else
+		shouldBeO = false;
+	ASSERT_TRUE(shouldBeO);
+}
+
+TEST(TicTacToeBoardTest, rowOutOfBoundsFirstCoordinate)
+{
+	TicTacToeBoard test;
+	Piece place = test.placePiece(3, 2);
+	bool shouldBeInvalid;
+	if(place == Invalid)
+		shouldBeInvalid = true;
+	else
+		shouldBeInvalid = false;
+	ASSERT_TRUE(shouldBeInvalid);
+}
+/*
+TEST(TicTacToeBoardTest, outOfBoundsSecondCoordinate)
+{
+	TicTacToeBoard test;
+	Piece place = test.placePiece(1, 3);
+	bool shouldBeInvalid;
+	if(place == Invalid)
+		shouldBeInvalid = true;
+	else
+		shouldBeInvalid = false;
+	ASSERT_TRUE(shouldBeInvalid);
+}
+
+TEST(TicTacToeBoardTest, bothOutOfBounds)
+{
+	TicTacToeBoard test;
+	Piece place = test.placePiece(8, 3);
+	bool shouldBeInvalid;
+	if(place == Invalid)
+		shouldBeInvalid = true;
+	else
+		shouldBeInvalid = false;
+	ASSERT_TRUE(shouldBeInvalid);
+}
+*/
